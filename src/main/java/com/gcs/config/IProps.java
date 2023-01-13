5647cd27c9d644d3d74838b5d23b0c334990a5aa
... <...>
2023-01-13 15:02:48 -0500

    adding iprops

---
+++ src/main/java/com/gcs/config/IProps.java
@@ -0,0 +1,28 @@
+/**
+ * Author: kgoldstein
+ * Date: Jan 8, 2023
+ * Terms: Expressly forbidden for use without written consent from the author
+ */
+
+
+
+
+
+package com.gcs.config;
+
+
+
+
+
+import java.util.HashMap;
+
+
+
+
+
+public interface IProps
+{
+
+    public HashMap<String, String> toHashMap();
+
+}
